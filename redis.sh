d684afe8b280ab489d17042d7c9ccd39294ff6cd
Pradeep-kanta <kantapradeep07@gmail.com>
2024-02-15 18:08:57 +0530

    roboshop scripts

---
+++ redis.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $LOGFILE
+VALIDATE $? "installing remirepo"
+
+dnf module enable redis:remi-6.2 -y &>> $LOGFILE
+VALIDATE $? "enabling redis remi"
+
+dnf install redis -y &>> $LOGFILE
+VALIDATE $? "installing redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf &>> $LOGFILE
+VALIDATE $? "providing remote access"
+
+systemctl enable redis &>> $LOGFILE
+VALIDATE $? "enabling redis"
+
+systemctl start redis &>> $LOGFILE
+VALIDATE $? "starting redis"
